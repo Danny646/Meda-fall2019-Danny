c8604aebc687196e4084ceac3ca157de96a60d74
student <student@PATC20.local>
2019-09-26 13:03:54 -0700

    javascript

---
+++ node_test.js
@@ -0,0 +1,95 @@
+
+
+
+// console.log ("Hello World How are you!")
+
+" This is a String of Cahracters";
+"this is also a string";
+"yet another string";
+// number data type
+"1920"; 
+"false"
+// Boolean
+true;
+false;
+
+// Creating a variable;
+//Syntax: var nameOfVariable;
+var messsage; 
+message = "This is stored inside of a virable!";
+// Grab the valeu of the message variable and send it to the console. 
+console.log (message); 
+console.log (message);
+// Variables can only hold one value at a time, so we are overwriting the previous message when we reassign.
+message= "I overwrote the previous text in message variable";
+//Becasue this console.log () happened after the update we will get the new value to the console. 
+console.log (message);
+
+var message=$. 
+ var= nameOfVaroiable
+var= numberof122
+myvariable
+vardollar
+var=myvariable14956
+var=$
+
+
+var variable1 ="string"
+var variable2= 1000009;
+var variable3 = false;
+var importantvar= 32;
+// End of basic Javascript. 
+var value1 = 100; 
+var value= value1; 
+console.log (value2);
+
+// Math Operators. 
+//+Addition
+//-Subtraction 
+//*Multplication 
+//%Modulo
+
+var sum = 39 + 74 + 1 +4 +300 +3999;
+var difference = 100 -30;
+var product = 100 * 2;
+// Good use of concation. 
+var username= "Eduardo Garcia":
+var sentencepart1 "Hello There,";
+var sentencePart2 ",Hello how are you doing?";
+var complete = senntencPart1 + userName + sentencePart2;
+console.log (complete);
+// Using variables to do math on changing data. 
+var num1 =100;
+var num2 = 40; 
+
+var sum1 = num1 +num2; 
+var difference1= num1 -num2;
+var difference2 =num2-num1;
+
+
+//Redifining Variables. 
+
+var createdVariable; 
+createdVariable =100;
+
+//created variable was already  defined;
+var createdVariable = 100;
+
+var someValue = 10-30 /6*3
+
+var otherValue= 10+(10 ( (3+4) (4*2)));
+
+//If else Statement
+if (true) {
+    console.log ("this is true!");
+} 
+
+//Conditinal operators
+/*/ == both sides are equal 
+>>Left side is equal or bigger 
+<=Left side equal or smaller 
+<Left side is smaller 
+>Right side is bigger 
+!=Not equal to 
+!Flip Boolean 
+*/
